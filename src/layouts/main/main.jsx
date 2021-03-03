18b96edebb19d2b902f49b56559579a980c0c896
AndrewKirpanev <spacewebdeveloper@gmail.com>
2021-03-03 23:42:53 +1000

    feat: add SEO for pages

--- src/layouts/main/main.jsx
+++ src/layouts/main/main.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 import Header from 'components/shared/header';
 import Footer from 'components/shared/footer';
+import SEO from 'components/shared/seo';
 
 const MainLayout = ({ children }) => (
   <>
+    <SEO />
     <Header />
     <main>{children}</main>
     <Footer />
--- src/utils/create-meta-image-path.js
+++ src/utils/create-meta-image-path.js
@@ -1,4 +1,9 @@
 // seo-component-specific function that creates correct path for the og:image
 export default function createMetaImagePath(image, siteUrl) {
-  return siteUrl + image.localFile.childImageSharp.fixed.src;
+  switch (typeof image) {
+    case 'object':
+      return siteUrl + image.childImageSharp.fluid.src;
+    default:
+      return siteUrl + image;
+  }
 }
