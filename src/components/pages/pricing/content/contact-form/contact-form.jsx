9c6e5e7111d3eb7de8ec890521efd0ef628b91ba
AndrewKirpanev <spacewebdeveloper@gmail.com>
2021-03-22 21:43:08 +1000

    chore: add small adjustment

--- src/components/pages/pricing/content/contact-form/contact-form.jsx
+++ src/components/pages/pricing/content/contact-form/contact-form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
