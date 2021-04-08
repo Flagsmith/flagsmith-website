9c92f882cf06af3b6604d80873caead4a4499a38
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-08 17:50:51 +0300

    feat(components): add responsive styles for Options in Pricing

--- src/components/pages/pricing/content/options/options.jsx
+++ src/components/pages/pricing/content/options/options.jsx
@@ -8,33 +8,34 @@ import Heading from 'components/shared/heading';
 import IconCheck from './images/check.inline.svg';
 import styles from './options.module.scss';
 
-
 const cx = classNames.bind(styles);
 
 const Options = ({ items }) => (
   <div className={cx('wrapper')}>
-    {items.map(({ title, theme, features }, index) => (
-      <div className={cx('item', theme)} key={index}>
-        <Heading className={cx('title')} tag="h3" size="lg" color={theme}>
-          {title}
-        </Heading>
+    <div className={cx('items-wrapper')}>
+      {items.map(({ title, theme, features }, index) => (
+        <div className={cx('item', theme)} key={index}>
+          <Heading className={cx('title')} tag="h3" size="lg" color={theme}>
+            {title}
+          </Heading>
 
-        <div className={cx('image')} />
-        <Button className={cx('button')} theme={theme} to="#contactForm">
-          Contact us
-        </Button>
-        <div className={cx('content')}>
-          <ul className={cx('features')}>
-            {features.map(({ text }, index) => (
-              <li key={index}>
-                <IconCheck />
-                {text}
-              </li>
-            ))}
-          </ul>
+          <div className={cx('image')} />
+          <Button className={cx('button')} theme={theme} to="#contactForm">
+            Contact us
+          </Button>
+          <div className={cx('content')}>
+            <ul className={cx('features')}>
+              {features.map(({ text }, index) => (
+                <li key={index}>
+                  <IconCheck />
+                  {text}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    ))}
+      ))}
+    </div>
   </div>
 );
 
