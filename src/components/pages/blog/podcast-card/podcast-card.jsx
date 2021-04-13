8bfe7dfb95eed375eadc5411c6b93707807cbd17
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-13 19:45:48 +0300

    feat(components): add PodcastCard for Blog page

---
+++ src/components/pages/blog/podcast-card/podcast-card.jsx
@@ -0,0 +1,58 @@
+import classNames from 'classnames/bind';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Button from 'components/shared/button';
+import Heading from 'components/shared/heading';
+
+import IconPlay from './images/play.inline.svg';
+import SoundWave from './images/sound-wave.inline.svg';
+import styles from './podcast-card.module.scss';
+
+const cx = classNames.bind(styles);
+
+const PodcastCard = ({ title, episode, duration, button }) => {
+  const { playerIllustration } = useStaticQuery(graphql`
+    query {
+      playerIllustration: file(
+        relativePath: { eq: "pages/blog/podcast-card/player-illustration.jpg" }
+      ) {
+        childImageSharp {
+          gatsbyImageData(width: 1010)
+        }
+      }
+    }
+  `);
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('head')}>
+        <IconPlay className={cx('icon')} />
+        <div className={cx('title')}>
+          <Heading tag="h2" size="lg" color="tertiary">
+            {title}
+          </Heading>
+          <span className={cx('episode')}>Eps.{episode}</span>
+          <span className={cx()}>{duration}</span>
+        </div>
+      </div>
+      <SoundWave className={cx('wave')} />
+      <Button className={cx('button')} to={button.url} theme="accent-tertiary">
+        {button.title}
+      </Button>
+      <GatsbyImage
+        className={cx('illustration')}
+        image={getImage(playerIllustration)}
+        alt=""
+        aria-hidden
+      />
+    </div>
+  );
+};
+
+PodcastCard.propTypes = {};
+
+PodcastCard.defaultProps = {};
+
+export default PodcastCard;
