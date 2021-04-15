220211d35f97653807e05a97b7d81b8c5b3b2e18
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-15 16:51:12 +0300

    feat(components): send email to GravityForm

--- src/components/shared/subscribe/subscribe.jsx
+++ src/components/shared/subscribe/subscribe.jsx
@@ -3,6 +3,9 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import sendGravityFormData from 'utils/send-gravity-form-data';
 
 import Button from '../button';
 import Heading from '../heading';
@@ -11,7 +14,22 @@ import styles from './subscribe.module.scss';
 
 const cx = classNames.bind(styles);
 
+const FORM_ID = 4;
+
 const Subscribe = ({ title, description, emailPlaceholder, button }) => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (values) => {
+    try {
+      sendGravityFormData(FORM_ID, {
+        input_1: values.email,
+      }).then(() => {
+        reset();
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   const { subscribeIllustration } = useStaticQuery(graphql`
     query {
       subscribeIllustration: file(
@@ -31,9 +49,15 @@ const Subscribe = ({ title, description, emailPlaceholder, button }) => {
             {title}
           </Heading>
           <div className={cx('description')} dangerouslySetInnerHTML={{ __html: description }} />
-          <form className={cx('form')}>
-            <input type="email" placeholder={emailPlaceholder} />
-            <Button className={cx('button')} to={button.url}>
+          <form className={cx('form')} noValidate onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="email"
+              name="email"
+              autoComplete="email"
+              placeholder={emailPlaceholder}
+              ref={register}
+            />
+            <Button className={cx('button')} type="submit">
               {button.title}
             </Button>
           </form>
