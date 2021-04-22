246af023ab5de888eb0c4ea893ebbf9eda42c608
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-22 16:24:19 +0300

    feat(components): add Audio to PodcastsList

--- src/components/pages/podcasts/podcasts-list/item/item.jsx
+++ src/components/pages/podcasts/podcasts-list/item/item.jsx
@@ -4,36 +4,40 @@ import React from 'react';
 
 import Heading from 'components/shared/heading';
 import Link from 'components/shared/link';
-import useAudio from 'hooks/use-audio';
 
+import Audio from './audio';
 import styles from './item.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Item = ({ podcastNumber, title, content, acf: { description }, url }) => {
-  const parsed = new DOMParser().parseFromString(content, 'text/html');
-  const audioElement = parsed.querySelector('audio');
-
-  const audio = new Audio(audioElement.src);
-  const { audioDuration } = useAudio(audio);
-
-  return (
-    <div className={cx('wrapper')}>
-      <Heading className={cx('title')} tag="h2" size="lg">
-        <span className={cx('number')}>#{podcastNumber}</span> {title}
-      </Heading>
-      <span className={cx('duration')}>{audioDuration}</span>
-      <p className={cx('description')}>{description}</p>
-      <Link to={url} withArrow>
-        Read More
-      </Link>
-    </div>
-  );
-};
+const Item = ({
+  podcastNumber,
+  title,
+  acf: { description },
+  url,
+  audioUrl,
+  isCurrent,
+  onStartPlay,
+}) => (
+  <div className={cx('wrapper')}>
+    <Heading className={cx('title')} tag="h2" size="lg">
+      <span className={cx('number')}>#{podcastNumber}</span> {title}
+    </Heading>
+    <p className={cx('description')}>{description}</p>
+    <Audio
+      audioUrl={audioUrl}
+      isCurrent={isCurrent}
+      podcastNumber={podcastNumber}
+      onStartPlay={onStartPlay}
+    />
+    <Link className={cx('link')} to={url} withArrow>
+      View Transcription
+    </Link>
+  </div>
+);
 Item.propTypes = {
   podcastNumber: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
   acf: PropTypes.shape({
     description: PropTypes.string.isRequired,
   }).isRequired,
