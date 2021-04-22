b2a7ad0aa813104c989b844e03c0198906c9b43e
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-22 16:22:35 +0300

    feat(components): add component Audio

--- src/hooks/use-audio.js
+++ src/hooks/use-audio.js
@@ -1,18 +1,54 @@
 import { useState, useEffect } from 'react';
 
-const useAudio = (audio) => {
-  const [audioDuration, setAudioDuration] = useState('');
+const useAudio = (audioUrl) => {
+  const [audioEl, setAudioEl] = useState(null);
+  const [duration, setDuration] = useState();
+  const [currentTime, setCurrentTime] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [clickedTime, setClickedTime] = useState();
+  const [isMuted, setIsMuted] = useState(false);
 
-  const getAudioDuration = (e) => {
-    const { duration } = e.target;
-    const timeDuration = `${Math.floor(duration / 60)}:${(duration % 60).toFixed()}`;
-    setAudioDuration(timeDuration);
-  };
   useEffect(() => {
-    audio.addEventListener('loadedmetadata', getAudioDuration);
-    return () => audio.removeEventListener('loadedmetadata', getAudioDuration);
-  });
-  return { audioDuration };
+    setAudioEl(new Audio(audioUrl));
+  }, [audioUrl]);
+
+  useEffect(() => {
+    if (!audioEl) {
+      return;
+    }
+    const getAudioData = (e) => {
+      const { duration } = e.target;
+      setDuration(duration);
+      setCurrentTime(audioEl.currentTime);
+    };
+    const setAudioTime = () => setCurrentTime(audioEl.currentTime);
+
+    audioEl.addEventListener('loadedmetadata', getAudioData);
+    audioEl.addEventListener('timeupdate', setAudioTime);
+
+    if (playing) {
+      audioEl.play();
+    } else {
+      audioEl.pause();
+    }
+
+    if (isMuted) {
+      audioEl.muted = true;
+    } else {
+      audioEl.muted = false;
+    }
+
+    if (clickedTime && clickedTime !== currentTime) {
+      audioEl.currentTime = clickedTime;
+      setClickedTime(null);
+    }
+
+    return () => {
+      audioEl.removeEventListener('loadedmetadata', getAudioData);
+      audioEl.removeEventListener('timeupdate', setAudioTime);
+    };
+  }, [audioEl, clickedTime, currentTime, isMuted, playing]);
+  return { duration, currentTime, playing, setPlaying, setClickedTime, isMuted, setIsMuted };
 };
 
 export default useAudio;
