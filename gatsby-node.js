f5659ec763d69589daabdd558ad505b556038b51
vannnyle@gmail.com <vannnyle@gmail.com>
2021-04-26 14:54:11 +0300

    refactor: remove unneeded content from blog post context

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,6 @@ const filterNonRootItems = require('./src/utils/filter-non-root-items');
 const SUPPORTED_MENU_TYPES = ['header', 'footer', 'mobile'];
 const POSTS_PER_PAGE = 9;
 
-// removes all the spaces from a string
-// stripSpaces(string: String) -> String
-const stripSpaces = (string) => string.replace(/\s+/g, ' ');
-
 async function createRedirects({ graphql, actions }) {
   const { createRedirect } = actions;
   const result = await graphql(`
@@ -281,7 +277,6 @@ async function createPosts({ graphql, actions, reporter, menus, sharedBlocks }) 
       allWpPost {
         nodes {
           id
-          content
           uri
         }
       }
@@ -293,19 +288,14 @@ async function createPosts({ graphql, actions, reporter, menus, sharedBlocks }) 
   }
   const posts = result.data.allWpPost.nodes;
 
-  posts.forEach(({ id, content, uri }) => {
+  posts.forEach(({ id, uri }) => {
     const templatePath = path.resolve('./src/templates/blog-post.jsx');
 
     const context = {
       id,
       menus,
-      content,
       sharedBlocks,
     };
-
-    if (content) {
-      context.content = stripSpaces(content);
-    }
 
     if (fs.existsSync(templatePath)) {
       createPage({
