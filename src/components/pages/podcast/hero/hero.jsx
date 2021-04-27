8f239dbc19ae6d84bb3deb19cb15a4ca918a7c8a
normuradoff <normuradoff1@gmail.com>
2021-04-27 17:03:06 +0500

    refactor(images): update images in Hero component

--- src/components/pages/podcast/hero/hero.jsx
+++ src/components/pages/podcast/hero/hero.jsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames/bind';
-import { graphql, useStaticQuery } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -7,26 +6,13 @@ import React from 'react';
 import Avatar from '../avatar';
 
 import styles from './hero.module.scss';
+import leftBackground from './images/left-background.svg';
+import rightBackground from './images/right-background.svg';
 
 const cx = classNames.bind(styles);
 
 const Hero = (props) => {
   const { host, guest, logo, quote } = props;
-  const { leftBackground, rightBackground } = useStaticQuery(graphql`
-    query {
-      leftBackground: file(relativePath: { eq: "pages/podcast/hero/left-background.jpg" }) {
-        childImageSharp {
-          gatsbyImageData(width: 767)
-        }
-      }
-      rightBackground: file(relativePath: { eq: "pages/podcast/hero/right-background.jpg" }) {
-        childImageSharp {
-          gatsbyImageData(width: 767)
-        }
-      }
-    }
-  `);
-
   return (
     <section className={cx('wrapper')}>
       <div className={cx('aspect-ratio')}>
@@ -44,10 +30,10 @@ const Hero = (props) => {
           <Avatar additionalClassName={cx('avatar', 'host')} {...host} position="Host Interview" />
           <Avatar additionalClassName={cx('avatar', 'guest')} theme="dark" isReversed {...guest} />
           <div className={cx('background', 'left')}>
-            <GatsbyImage image={getImage(leftBackground)} alt="" />
+            <img src={leftBackground} alt="" />
           </div>
           <div className={cx('background', 'right')}>
-            <GatsbyImage fadeIn="false" image={getImage(rightBackground)} alt="" />
+            <img src={rightBackground} alt="" />
           </div>
         </div>
       </div>
