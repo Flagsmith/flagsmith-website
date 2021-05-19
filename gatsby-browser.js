b0a1058a1987b63e56ebd766b9c714ecc68f1d28
vannnyle@gmail.com <vannnyle@gmail.com>
2021-05-19 16:51:51 +0300

    chore: unregister service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,12 @@
 
 // You can delete this file if you're not using it
 require('./src/styles/app.scss');
+
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
