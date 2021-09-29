ec7ab79d997e5c60db6b74195bd7d654c9fc09a9
vannnyle@gmail.com <vannnyle@gmail.com>
2021-09-29 16:14:15 +0300

    chore: update default color in gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1e0d26',
+        theme_color: '#1e0d26',
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
