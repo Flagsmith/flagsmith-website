7e8d7020152472b906738e0dacf7fd761e65405d
Ben Rometsch <ben.rometsch@gmail.com>
2022-09-05 18:13:47 +0100

    chore: sort env var

--- api/contact-us.js
+++ api/contact-us.js
@@ -1,11 +1,11 @@
 const pipedrive = require('pipedrive');
 
 const defaultClient = pipedrive.ApiClient.instance;
-const apiToken = defaultClient.authentications.api_key;
-apiToken.apiKey = '${process.env.PIPEDRIVE_API_KEY}';
 const pipedrivePersonsApi = new pipedrive.PersonsApi();
 const pipedriveDealsApi = new pipedrive.DealsApi();
 const pipedriveNotesApi = new pipedrive.NotesApi();
+const apiToken = defaultClient.authentications.api_key;
+apiToken.apiKey = process.env.PIPEDRIVE_API_KEY;
 
 export default async function handler(request, response) {
   const newPerson = pipedrive.NewPerson.constructFromObject({
@@ -26,29 +26,29 @@ export default async function handler(request, response) {
   });
   pipedrivePersonsApi.addPerson(newPerson).then(
     (personData) => {
-      console.log(`pipedrivePersonsApi called successfully. Returned data: ${  personData}`);
+      console.log(`pipedrivePersonsApi called successfully. Returned data: ${personData}`);
 
       const newDeal = pipedrive.NewDeal.constructFromObject({
-        title: `New ${  personData.data.primary_email  } Deal`,
+        title: `New ${personData.data.primary_email} Deal`,
         person_id: personData.data.id,
       });
 
       console.log('Adding Deal.');
       pipedriveDealsApi.addDeal(newDeal).then(
         (dealData) => {
-          console.log(`pipedriveDealsApi called successfully. Returned data: ${  dealData}`);
+          console.log(`pipedriveDealsApi called successfully. Returned data: ${dealData}`);
 
           const newNote = pipedrive.AddNoteRequest.constructFromObject({
             deal_id: dealData.data.id,
-            content:
-              `From Website Contact Us Form: ${ 
-              request.body.input_5 != null ? request.body.input_5 : 'No note supplied'}`,
+            content: `From Website Contact Us Form: ${
+              request.body.input_5 != null ? request.body.input_5 : 'No note supplied'
+            }`,
           });
 
           console.log('Adding Note.');
           pipedriveNotesApi.addNote(newNote).then(
             (noteData) => {
-              console.log(`pipedriveNotesApi called successfully. Returned data: ${  noteData}`);
+              console.log(`pipedriveNotesApi called successfully. Returned data: ${noteData}`);
               response.status(200).json({
                 body: noteData,
               });
