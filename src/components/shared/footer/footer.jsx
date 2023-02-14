d67f8b2278fe87bd5dce70dbd7831cdfdfaeed28
Ben Rometsch <ben.rometsch@gmail.com>
2023-02-14 19:04:35 +0000

    Added soc2 footer

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -1,4 +1,6 @@
 import classNames from 'classnames/bind';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React, { useContext } from 'react';
 
 import Link from 'components/shared/link';
@@ -15,12 +17,27 @@ const Footer = () => {
     menus: { footerMenuItems: menuItems },
   } = useContext(MainContext);
 
+  const { soc2 } = useStaticQuery(graphql`
+    query {
+      soc2: file(relativePath: { eq: "shared/soc2.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 200)
+        }
+      }
+    }
+  `);
+
   return (
     <footer className={cx('wrapper')}>
       <div className={cx('container', 'inner')}>
         <div className={cx('logo-wrapper')}>
           <Link to="/">
             <Logo />
+          </Link>
+        </div>
+        <div className={cx('soc2-wrapper')}>
+          <Link to="/">
+            <GatsbyImage image={getImage(soc2)} alt="Soc2 Compliant" />
           </Link>
         </div>
         <div className={cx('menu')}>
