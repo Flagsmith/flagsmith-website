e84f9a64d6d0eb1c9906b821ad4608306e56d24d
Ben Rometsch <ben.rometsch@gmail.com>
2023-03-10 13:40:20 +0000

    Moved pipedrive to leads

--- api/contact-us.js
+++ api/contact-us.js
@@ -2,7 +2,7 @@ const pipedrive = require('pipedrive');
 
 const defaultClient = pipedrive.ApiClient.instance;
 const pipedrivePersonsApi = new pipedrive.PersonsApi();
-const pipedriveDealsApi = new pipedrive.DealsApi();
+const pipedriveLeadsApi = new pipedrive.LeadsApi();
 const pipedriveNotesApi = new pipedrive.NotesApi();
 const apiToken = defaultClient.authentications.api_key;
 apiToken.apiKey = process.env.PIPEDRIVE_API_KEY;
@@ -24,22 +24,23 @@ export default async function handler(request, response) {
       },
     ],
   });
+
   pipedrivePersonsApi.addPerson(newPerson).then(
     (personData) => {
       console.log(`pipedrivePersonsApi called successfully. Returned data: ${personData}`);
 
-      const newDeal = pipedrive.NewDeal.constructFromObject({
-        title: `New ${personData.data.primary_email} Deal`,
+      const newLead = pipedrive.AddLeadRequest.constructFromObject({
+        title: `New ${personData.data.primary_email} Lead`,
         person_id: personData.data.id,
       });
 
-      console.log('Adding Deal.');
-      pipedriveDealsApi.addDeal(newDeal).then(
-        (dealData) => {
-          console.log(`pipedriveDealsApi called successfully. Returned data: ${dealData}`);
+      console.log('Adding Lead.');
+      pipedriveLeadsApi.addLead(newLead).then(
+        (leadData) => {
+          console.log(`pipedriveLeadsApi called successfully. Returned data: ${leadData}`);
 
           const newNote = pipedrive.AddNoteRequest.constructFromObject({
-            deal_id: dealData.data.id,
+            lead_id: leadData.data.id,
             content: `From Website Contact Us Form: ${
               request.body.input_5 != null ? request.body.input_5 : 'No note supplied'
             }`,
@@ -62,7 +63,7 @@ export default async function handler(request, response) {
           );
         },
         (error) => {
-          console.log('pipedriveDealsApi called error');
+          console.log('pipedriveLeadsApi called error');
         }
       );
     },
